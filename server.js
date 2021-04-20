e59e7dfd8246a2d9bec5536e847a725ece8e0d12
KaleTEvans <kaletevans@gmail.com>
2021-04-20 12:27:04 -0500

    Add heroku

---
+++ server.js
@@ -0,0 +1,49 @@
+const { animals } = require('./data/animals');
+
+const express = require('express');
+
+const app = express();
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    // save the anmals array as filteredResults
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        // save personality traits as a dedicated array
+        // if personality traits is a string save it in a new array
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        // loop through each trait if in array
+        personalityTraitsArray.forEach(trait => {
+            // check the trait against each animal in the array
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+})
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
